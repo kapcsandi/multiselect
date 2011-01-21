4530f424e7355765f14985643f0df6cd6eea7a63
Kapcsándi István <istvan.kapcsandi@gmail.com>
2011-01-21 13:31:31 +0100

    Added Hungarian translation

---
+++ js/locale/ui-multiselect-hu.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale hu, hu-HU
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Mind',
+	removeAll:'Egyiksem',
+	itemsCount:'kiválasztva'
+});
